4ed7655b59c7139c9627e994329225a784158119
thomas <thomas>
2006-07-11 10:02:52 +0000

    Some updates...

--- src/soap_server.py
+++ src/soap_server.py
@@ -19,8 +19,11 @@ def shutdownVPS(vpsname):
 def listStartedVPS():
   return "ok"
 
+def auth(userid, password, mode='clear', auth=None):
+  return userid
 
 soapserver = SOAPpy.SOAPServer(("dtc.xen650202.gplhost.com", server_port))
+soapserver.registerFunction(auth)
 soapserver.registerFunction(testVPSServer)
 soapserver.registerFunction(startVPS)
 soapserver.registerFunction(destroyVPS)
