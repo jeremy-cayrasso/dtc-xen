5aaeb25f502f049fa889b3fd7c0af6620c678e51
tusker <tusker>
2006-07-22 08:39:48 +0000

    Add examples for start VPS

--- src/soap_client.php
+++ src/soap_client.php
@@ -1,18 +1,28 @@
 <?php
 
-$host = "localhost";
 $port = 8089;
-// $host = "dtc.xen650202.gplhost.com";
+// $host = "localhost";
+$host = "dtc.xen650202.gplhost.com";
 // $host = "mirror.tusker.net";
+// $host = "node6502.gplhost.com";
 
 require("nusoap.php");
 
 $soap_client = new soapclient("https://$host:$port/");
 $soap_client->setCredentials("JohnDoe", "JDsPassword");
-$r = $soap_client->call("testVPSServer","","","","");
+$r = $soap_client->call("listStartedVPS","","","","");
+
 $err = $soap_client->getError();
 if(!$err){
-  echo "Result: ".$r;
+  echo "Result: ".print_r($r);
+}else{
+  echo "Error: ".$err;
+}
+
+$r = $soap_client->call("startVPS",array("vpsname" => "xen13"),"","","");
+$err = $soap_client->getError();
+if(!$err){
+  echo "Result: ".print_r($r);
 }else{
   echo "Error: ".$err;
 }
