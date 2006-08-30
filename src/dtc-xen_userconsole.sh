54b4592cabdffaf10e48fd026b29914251e37756
thomas <thomas>
2006-08-30 03:54:08 +0000

    Added a check to see if there is a parameter in the ssh (a shell command), and disable the access for security reasons.

--- src/dtc-xen_userconsole.sh
+++ src/dtc-xen_userconsole.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 echo "Welcome to dtc-xen ssh console!"
-sleep 2
+sleep 1
+
+if [ -n "$1" ]; then
+	echo "Sorry, no shell commands allowed"
+	exit 1
+fi
 
 sudo /usr/sbin/xm console $USER
