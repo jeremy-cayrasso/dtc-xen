bce3af9ec80b9616487717d8014e042c9693f6ca
Thomas Goirand <thomas@goirand.fr>
2007-11-02 17:57:52 +0800

    Another git try, please ignore if you are reading this.

--- src/dtc-soap-server.py
+++ src/dtc-soap-server.py
@@ -49,6 +49,7 @@ except:
 # FIXME WHY ARE WE SILENCING THIS EXCEPTION?
 # Thomas: Because it's just a check for xen2 vs xen3
 # Extra useless git comment: to be removed.
+# And another one.
 
 # Checking for Xen version
 logging.debug("Checking for Xen version")
