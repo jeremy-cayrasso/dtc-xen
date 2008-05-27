486e2e75141479069f6e3a06bc6a3388cb539843
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2008-05-27 16:10:53 -0500

    Killed xenupdtae and xengraph cron jobs

--- debian/dtc-xen.cron.d
+++
@@ -1,3 +0,0 @@
-* * * * * root	[ -f /usr/share/dtc-xen/graph/xenupdate.py ] && /usr/share/dtc-xen/graph/xenupdate.py /var/lib/dtc-xen/rrds >/dev/null
-2,7,12,17,22,27,32,37,42,47,52,57 * * * * root	[ -f /usr/share/dtc-xen/graph/xennetgraph.py ] && /usr/share/dtc-xen/graph/xennetgraph.py /var/lib/dtc-xen/rrds /var/www/dtc-xen/graph/ >/dev/null
-4,9,14,19,24,29,34,39,44,49,54,59 * * * * root	[ -f /usr/share/dtc-xen/graph/xencpugraph.py ] && /usr/share/dtc-xen/graph/xencpugraph.py /var/lib/dtc-xen/rrds /var/www/dtc-xen/graph/ >/dev/null
