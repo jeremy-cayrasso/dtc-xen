1b12a3e80b547c6347aa3c1dd1eaaec66e47cb69
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2008-05-27 11:33:52 -0500

    i/o data collection and xenupdate.py rewrite

--- panel/graph/xenupdate.py
+++ panel/graph/xenupdate.py
@@ -3,76 +3,100 @@ import sys
 import os
 import re
 import time
+from subprocess import Popen,call,PIPE
+from glob import glob
 
-if len(sys.argv)!=2:
-    print "Syntax: %s <rrdbasename>"%sys.argv[0]
-    sys.exit(1)
+def check_call(*args):
+	retval = call(*args)
+	if retval != 0: raise Exception, "call() failed with error %s: %s"%(retval,args)
+	return retval
 
-# first check for running xm list, and abort if there is one running already
-status=os.system("/usr/bin/pgrep -f \"xm list\"")
-# status will be 1 (ie, it couldn't find an xm list running)
-# status will be 0 (if there is one already running)
-if status == 0:
-        print "xm list is already running, bail out here"
-        sys.exit(1)
+tabsplitter = re.compile("[\t ]+").split
 
-now=str(int(time.time()))
-basename=sys.argv[1]
-data=os.popen2("/usr/sbin/xm list")[1].read()
-domains=data.split("\n")[1:-1]
+if len(sys.argv) < 2:
+    print "Syntax: %s <rrdbasename> [--test]"%sys.argv[0]
+    sys.exit(os.EX_USAGE)
+
+now = "%d"%time.time()
+basename = sys.argv[1]
+domains = (
+		tabsplitter(d.strip())
+		for d in Popen(["/usr/sbin/xm","list"], stdout=PIPE).communicate()[0].splitlines()[1:]
+		if d.strip()
+	)
 for domain in domains:
-    domainsplit = re.split("[\t ]+",domain)
-    if len(domainsplit) == 7:
-      name,id,mem,cpu,state,cputime,console=domainsplit
-    elif len(domainsplit) == 6:
-      name,id,mem,cpu,state,cputime=domainsplit
-    else:
-      print "Domain has unknown size of %d" % len(domainsplit)
-      print "Domain: %s" % domain
-      sys.exit(1)
+
+    name,id,mem,cpu,state,cputime=domain[0:6]
     # Log the CPU of each domain
     cputime=int(float(cputime)*1000)
-    cpurrd=basename+"/cpu-"+name+".rrd"
-    if not os.access(cpurrd,os.F_OK):
-      # 10 days of exact archive, 42 days of 1 hr RRD, 1000 days of 1 day RRD
-      os.system("rrdtool create "+cpurrd+" --step 60 DS:cpu:COUNTER:666:0:10000"+
-        " RRA:LAST:0.5:1:15000"+
-        " RRA:MIN:0.5:60:1000 RRA:MIN:0.5:1440:1000"+
-        " RRA:MAX:0.5:60:1000 RRA:MAX:0.5:1440:1000"+
-        " RRA:AVERAGE:0.5:60:1000 RRA:AVERAGE:0.5:1440:1000")
-    os.system("nice -n+20 rrdtool update %s %s:%s"%(cpurrd,now,cputime))
+    rrd=os.path.join(basename,"cpu-"+name+".rrd")
+    if not os.path.exists(rrd):
+	# 10 days of exact archive, 42 days of 1 hr RRD, 1000 days of 1 day RRD
+	check_call( [
+		"rrdtool",
+		"create",
+		rrd,
+		"--step","60",
+		"DS:cpu:COUNTER:666:0:10000",
+		"RRA:LAST:0.5:1:15000",
+		"RRA:MIN:0.5:60:1000","RRA:MIN:0.5:1440:1000",
+		"RRA:MAX:0.5:60:1000","RRA:MAX:0.5:1440:1000",
+		"RRA:AVERAGE:0.5:60:1000","RRA:AVERAGE:0.5:1440:1000",
+	] )
+    check_call( [ "nice","-n+20","rrdtool","update",rrd,"%s:%s"%(now,cputime) ] )
 
-    # Log the network usage of each domain
-    if name=="Domain-0":
-      continue
-#      devinfo=os.popen2("cat /proc/net/dev | grep 'eth0'")[1].read()
-    else:
-      devinfo=os.popen2("cat /proc/net/dev | grep 'vif"+id+"\.0'")[1].read()
-    rows=re.split("\n", devinfo)
-    inplusout=0
-    for row in rows:
-        columns=re.split("[\t ]+",row)
-        if (len(columns)==16):
-	        one,a,a,a,a,a,a,a,eight,a,a,a,a,a,a,a=columns
-	        one=re.split(":",one)[1]
-        elif (len(columns)==17):
-            a,one,a,a,a,a,a,a,a,eight,a,a,a,a,a,a,a=columns
-        elif (len(columns)==1):
-            # this is an empty row, continue
-            continue
-	else:
-            # this is an unknown row, continue
-	    print str(columns) + str(len(columns))
-            continue
-        # print name +' '+ id +' '+ one +' '+ eight
-        inplusout=inplusout + int(one) + int(eight)
-        # print name +' '+ ifname +' '+ str(inplusout)
-        netrrd=basename+"/net-"+name+".rrd"
-        if not os.access(netrrd,os.F_OK):
-           # 10 days of exact archive, 42 days of 1 hr RRD, 1000 days of 1 day RRD
-           os.system("rrdtool create "+netrrd+" --step 60 DS:bytes:COUNTER:600:0:12500000"+
-                     " RRA:LAST:0.5:1:15000"+
-                     " RRA:MIN:0.5:60:1000 RRA:MIN:0.5:1440:1000"+
-                     " RRA:MAX:0.5:60:1000 RRA:MAX:0.5:1440:1000"+
-                     " RRA:AVERAGE:0.5:60:1000 RRA:AVERAGE:0.5:1440:1000")
-        os.system("nice -n+20 rrdtool update %s %s:%s"%(netrrd,now,inplusout))
+    # if we're dealing with the dom0, neeeext...
+    if name == "Domain-0": continue
+     
+    # Log the network usage of each domU domain
+    inbytes,a,a,a,a,a,a,a,outbytes,a,a,a,a,a,a,a=tabsplitter(
+	[
+		re.sub(".*:","",o).strip()
+		for o in file("/proc/net/dev").readlines()
+		if o.startswith("vif%s.0:"%id)
+	] [0]
+    )
+    inplusout = int(inbytes) + int(outbytes)
+    rrd = os.path.join(basename,"net-"+name+".rrd")
+    if not os.path.exists(rrd):
+	# 10 days of exact archive, 42 days of 1 hr RRD, 1000 days of 1 day RRD
+	check_call( [
+		"rrdtool",
+		"create",
+		rrd,
+		"--step","60",
+		"DS:bytes:COUNTER:600:0:12500000",
+		"RRA:LAST:0.5:1:15000",
+		"RRA:MIN:0.5:60:1000","RRA:MIN:0.5:1440:1000",
+		"RRA:MAX:0.5:60:1000","RRA:MAX:0.5:1440:1000",
+		"RRA:AVERAGE:0.5:60:1000","RRA:AVERAGE:0.5:1440:1000",
+	] )
+    check_call( [ "nice","-n+20","rrdtool","update",rrd,"%s:%s"%(now,inplusout) ] )
+
+    # Log the block i/o usage of each domU domain
+    # FIXME: doesn't differentiate between swap and file i/o, or writes and reads
+    # FIXME: # ls /sys/devices/xen-backend/vbd-6-51712/statistics/
+    #br_req  oo_req  rd_req  rd_sect  wr_req  wr_sect
+    #rd_sect / wr_sect contain sectors, so if we can assume 512 bytes/sector then we could fill in the bytes fields.
+    # http://www.redhat.com/archives/libvir-list/2007-August/msg00224.html
+    iosectors = sum(
+    	( int(file(f).read()) for f in glob(
+			os.path.join("/","sys","devices","xen-backend","vbd-%s-*"%id,"statistics","*_sect")
+		)
+	)
+    )
+    rrd=os.path.join(basename,"iosectors-"+name+".rrd")
+    if not os.path.exists(rrd):
+	# 10 days of exact archive, 42 days of 1 hr RRD, 1000 days of 1 day RRD
+	check_call( [
+		"rrdtool",
+		"create",
+		rrd,
+		"--step","60",
+		"DS:bytes:COUNTER:600:0:12500000",
+		"RRA:LAST:0.5:1:15000",
+		"RRA:MIN:0.5:60:1000","RRA:MIN:0.5:1440:1000",
+		"RRA:MAX:0.5:60:1000","RRA:MAX:0.5:1440:1000",
+		"RRA:AVERAGE:0.5:60:1000","RRA:AVERAGE:0.5:1440:1000",
+	] )
+    check_call( [ "nice","-n+20","rrdtool","update",rrd,"%s:%s"%(now,iosectors) ] )
