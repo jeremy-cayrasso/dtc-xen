7c81dbd42885610e19bd9bd85cf28488e8495a1c
Thomas Goirand <thomas@goirand.fr>
2008-06-25 15:14:53 +0800

    Corrected the new I/O graph.

--- panel/graph/xeniograph.py
+++ panel/graph/xeniograph.py
@@ -16,16 +16,16 @@ def graph(basename, subname, seconds):
     rrds=[x for x in rrds if os.stat(x)[8] > (time.time()-seconds)]
 
     # cmdline="--imgformat PNG --unit % --vertical-label CPU%  "
-    cmdline="--imgformat PNG --vertical-label CPU%  "
+    cmdline="--imgformat PNG --vertical-label IO bytes "
     #cmdline="--imgformat PNG --unit % --vertical-label CPU% --rigid --lower-limit 0 --upper-limit 100 "
 
     for rrd,id in zip(rrds,range(len(rrds))):
-	cmdline+="DEF:id%draw=%s:cpu:LAST "%(id,rrd)
+	cmdline+="DEF:id%draw=%s:bytes:LAST "%(id,rrd)
 	cmdline+="CDEF:id%dpercent=id%draw,10,/ "%(id,id)
 	cmdline+="CDEF:id%dzero=id%dpercent,DUP,UN,EXC,0,EXC,IF "%(id,id)
     for rrd,id in zip(rrds,range(len(rrds))):
 	cmdline+="%s:id%dzero#%s:%s "%({0:"AREA"}.get(id,"STACK"),id,colors[id%len(colors)],rrd[len(basename):-4])
-	cmdline+="GPRINT:id%dpercent:AVERAGE:\"%s\" "%(id,"Average CPU usage\: %02.0lf%%\\j")
+	cmdline+="GPRINT:id%dpercent:AVERAGE:\"%s\" "%(id,"Average IO usage\: %02.0lf\\j")
 
     # print cmdline
     os.system("nice -n+20 rrdtool graph %sxen-io-%s.png --start -%d --end -69 %s > /dev/null"%(folder,subname,seconds,cmdline))
