cdd9541a771d8acd9f95ce6cd9160f69eec611e0
Thomas Goirand <thomas@goirand.fr>
2008-11-14 11:57:39 +0800

    Some more autodeploy stuffs...

--- src/dtc-panel_autodeploy.sh
+++ src/dtc-panel_autodeploy.sh
@@ -40,13 +40,30 @@ IP_ADDR=`ifconfig ${DEFAULT_IF} | grep 'inet addr' | sed 's/.\+inet addr:\([0-9.
 MKTEMP="mktemp -t"
 
 SETSEL_FILE=`${MKTEMP} DTC_AUTODEPLOY.XXXXXX` || exit 1
+
+# Set debconf back to Noninteractive, otherwise phpmyadmin is annoying...
+echo "debconf debconf/frontend select Noninteractive
+debconf debconf/frontend seen true
+debconf debconf/priority select medium
+debconf debconf/priority seen true
+debconf debconf-apt-progress/title string fake
+debconf debconf-apt-progress/title seen true
+debconf debconf-apt-progress/preparing string fake
+debconf debconf-apt-progress/preparing seen true" >${SETSEL_FILE}
+debconf-set-selections ${SETSEL_FILE}
+apt-get --reinstall install debconf
+
+# Copy our selection_config_file template file, and tweak it with correct values
 cp selection_config_file ${SETSEL_FILE}
 
 sed -i "s/__PASSWORD__/${PASSWORD}/g" ${SETSEL_FILE}
 sed -i "s/__DOMAIN_NAME__/${DOMAIN_NAME}/g" ${SETSEL_FILE}
 sed -i "s/__IP__ADDRESS__/${IP_ADDR}/g" ${SETSEL_FILE}
 
+# Set the values needed to setup DTC
 debconf-set-selections ${SETSEL_FILE}
+
+# Set the locales, otherwise postinst are printing a bunch of ugly warning messages
 echo "en_US.UTF-8 UTF-8
 en_US ISO-8859-1
 en_US.ISO-8859-15 ISO-8859-15
@@ -54,7 +71,20 @@ en_US.ISO-8859-15 ISO-8859-15
 export LANGUAGE="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
-update-locale LANG=en_US.UTF-8
 locale-gen
 apt-get --force-yes --assume-yes install dtc-toaster
+
+# Set debconf back to medium priority, using Dialog
+echo "debconf debconf/frontend select Dialog
+debconf debconf/frontend seen true
+debconf debconf/priority select medium
+debconf debconf/priority seen true
+debconf debconf-apt-progress/title string fake
+debconf debconf-apt-progress/title seen true
+debconf debconf-apt-progress/preparing string fake
+debconf debconf-apt-progress/preparing seen true" >${SETSEL_FILE}
+debconf-set-selections ${SETSEL_FILE}
+apt-get --reinstall install debconf
+
+# Finally start the dtc shell installer and we are done!
 /usr/share/dtc/admin/install/install
